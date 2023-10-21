6a5b07fc80efe9f8f342cb023c957589de0eeb94
perezromero5 <tomasperezromero5@gmail.com>
2023-10-21 02:10:50 -0300

    agregue promesas y mejore la sintaxis de js

--- scripts.js/contacto.js
+++ scripts.js/contacto.js
@@ -1,4 +1,4 @@
-let precioTotal = obtenerPrecioTotalGuardado()
+/*let precioTotal = obtenerPrecioTotalGuardado()
 const planForm = document.getElementById('planForm');
 const precioTotalSpan = document.getElementById('precioTotal');
 const calcularPrecioButton = document.getElementById('calcularPrecio');
@@ -45,4 +45,63 @@ function obtenerPrecioTotalGuardado() {
 function guardarPrecioTotal(precioTotal) {
     localStorage.setItem('precioTotalJSON', JSON.stringify(precioTotal));
 }
+*/
+const planForm = document.getElementById('planForm');
+const precioTotalSpan = document.getElementById('precioTotal');
+const calcularPrecioButton = document.getElementById('calcularPrecio');
 
+// Utilizamos una promesa para obtener el precio total guardado
+function obtenerPrecioTotalGuardado() {
+    return new Promise((resolve, reject) => {
+        const precioTotalJSON = localStorage.getItem('precioTotalJSON');
+        if (precioTotalJSON) {
+            resolve(parseFloat(JSON.parse(precioTotalJSON)));
+        } else {
+            resolve(0);
+        }
+    });
+}
+
+// Utilizamos una promesa para guardar el precio total
+function guardarPrecioTotal(precioTotal) {
+    return new Promise((resolve, reject) => {
+        localStorage.setItem('precioTotalJSON', JSON.stringify(precioTotal));
+        resolve(precioTotal);
+    });
+}
+
+calcularPrecioButton.addEventListener('click', () => {
+    const planCheckboxes = document.querySelectorAll('input[name="plan"]:checked');
+    const planesSeleccionados = Array.from(planCheckboxes).map(checkbox => checkbox.value);
+
+    const precios = {
+        "Gym 2 veces por semana": 3200,
+        "Gym 3 veces por semana": 3800,
+        "Pilates 2 veces por semana": 3000,
+        "Gym+pilates 2 veces por semana": 4500,
+        "Gym/pilates libre": 4800,
+    };
+
+    // Calculamos el precio total utilizando la función reduce
+    const nuevoPrecioTotal = planesSeleccionados.reduce((total, plan) => {
+        return total + (precios[plan] || 0);
+    }, 0);
+
+    // Guardamos el precio total utilizando la promesa
+    guardarPrecioTotal(nuevoPrecioTotal)
+        .then(precioTotal => {
+            precioTotalSpan.textContent = `$${precioTotal.toFixed(2)}`;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
+// Obtenemos el precio total utilizando la promesa
+obtenerPrecioTotalGuardado()
+    .then(precioTotal => {
+        precioTotalSpan.textContent = `$${precioTotal.toFixed(2)}`;
+    })
+    .catch(error => {
+        console.error(error);
+    });
